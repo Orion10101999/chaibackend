63f825ce5f7fd028a8cd5664f62e4dc7c0445a2b
Orion10101999 <142068663+Orion10101999@users.noreply.github.com>
2023-12-09 03:12:46 +0000

    data structure changes

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ import connectDB from './db/index.js';
 const app = express()
 dotenv.config({path:'./env'})
 connectDB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`Server is Running at port : ${process.env.PORT}`);
+  })
+  app.on("error",(error)=>{
+    console.log("ERRR", error);
+    throw error
+  })
+})
+.catch((err)=>{
+  console.log("MongoDB connection Failed",err);
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -22,7 +34,3 @@ app.get('/youtube', (req, res) => {
   res.send('<h2>chai aur code Youtube </h2>')
 })
 
-
-app.listen(process.env.PORT || 3200, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
-})
